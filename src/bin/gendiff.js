556975c687ca4de3fd61228733bdf0339e04f943
NickKrsk <ggd43@rambler.ru>
2020-01-29 21:35:30 +0700

    first

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -4,11 +4,24 @@ import program from 'commander';
 
 //const program = new commander.Command();
 
-program.version('0.0.1');
-
 program
+  .version('0.0.3')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f --format <type>', 'output format');
+
+/*program
   .option('-d, --debug', 'output extra debugging')
   .option('-s, --small', 'small pizza size')
-  .option('-p, --pizza-type <type>', 'flavour of pizza');
+  .option('-p, --pizza-type <type>', 'flavour of pizza');*/
 
+program.parse(process.argv);
+
+if (program.format) console.log(`- ${program.format}`);
+
+/*
+if (program.debug) console.log(program.opts());
+console.log('pizza details:');
+if (program.small) console.log('- small pizza size');
+if (program.pizzaType) console.log(`- ${program.pizzaType}`);
+*/
   console.log('hello');

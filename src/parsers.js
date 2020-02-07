1084d164422726881840a59af74c5bc31ee6b170
NickKrsk <ggd43@rambler.ru>
2020-02-07 17:24:23 +0700

    render

--- src/parsers.js
+++ src/parsers.js
@@ -1,38 +1,55 @@
 import _ from 'lodash';
 
-const render = (parsedArray) => {
-  if (parsedArray.length === 0) {
-    console.log('empty');
-    return '';
+const stringify = value => {
+
+  if (typeof (value) !== 'object') {
+    return value;
   }
+  const result = Object.keys(value).reduce((acc, key) => {
+    const res = value[key];
+    return [...acc, `${key}: ${res}`];
+  }, []).join('\n');
+  return `{\n${result}\n}`;
+};
+
+const convertDiffTypeToSymbol = diffType => {
+  switch(diffType) {
+    case 'add':
+      return '+';
+    case 'remove':
+      return '-';
+    default:
+      return ' ';
+  }
+};
+
+
+const render = (parsedArray, deep = 1) => {
+  const indent = "    ".repeat(deep);
   const resultArray = parsedArray.reduce((acc, node) => {
-    if (node.diff === 'add') {
-      acc.push(`+ ${node.name}: ${node.valueAfter}`);
+    const diffSymbol = convertDiffTypeToSymbol(node.diff);
+    if (node.children.length > 0) {
+      const deepValue = render(node.children, deep + 1);
+      return [...acc, `${indent}${diffSymbol} ${node.name}: {\n${deepValue}\n}`];
     }
+    const value = node.diff === 'remove' ? node.valueBefore : node.valueAfter;
+    const strValue = stringify(value);
 
-    if (node.diff === 'remove') {
-      acc.push(`- ${node.name}: ${node.valueBefore}`);
+    if (node.diff !== 'changed') {
+      return [...acc, `${indent}${diffSymbol} ${node.name}: ${strValue}`];
     }
-
-    if (node.diff === 'equals') {
-      acc.push(`${node.name}: ${node.valueAfter}`);
-    }
-    
-    if (node.children !== []) {
-      const deepValue = render(node.children);
-      acc.push(`    ${deepValue}`);
-    }
-    
-    return acc;
+    const valueBefore = stringify(node.valueBefore);
+    const valueAfter = stringify(node.valueAfter);
+    return [...acc, `${indent}- ${node.name}: ${valueBefore}`, `+ ${node.name}: ${valueAfter}`];
   }, []);
 
   const resultStr = resultArray.join('\n');
-  return `{\n${resultStr}\n}`;
+  return resultStr;
 };
+
 
 const parse = (objBefore, objAfter) => {
   const allKeys = Object.keys({ ...objBefore, ...objAfter }).sort();
-  //console.log(allKeys);
   const parsedArray = allKeys.reduce((acc, key) => {
     const node = {
       name: key,
@@ -49,7 +66,7 @@ const parse = (objBefore, objAfter) => {
       node.diff = 'add';
 
       if (typeof (valueAfter) === 'object') {
-        node.children.add(valueAfter);
+        node.children = parse(valueAfter, valueAfter);
       }
     } else if (!_.has(objAfter, key)) {
       // 2. key is not found in data2`;
@@ -57,9 +74,9 @@ const parse = (objBefore, objAfter) => {
       node.valueBefore = valueBefore;
       node.diff = 'remove';
       if (typeof (valueBefore) === 'object') {
-        node.children.add(valueBefore);
+        node.children = parse(valueBefore, valueBefore);
       }
-    } else if (objBefore[key] !== objAfter[key]) {
+    } else {
       // 3. key is found in both objects 
       // key a different in data1 and data2`);
       const valueAfter = objAfter[key];
@@ -73,21 +90,23 @@ const parse = (objBefore, objAfter) => {
         node.diff = valueAfter === valueBefore ? 'equals' : 'changed';
       } else if (typeof (valueBefore) === 'object' && typeof (valueAfter) === 'object') {
         node.diff = 'deep';
-        node.children = parse(valueBefore, valueAfter);
+        const child = parse(valueBefore, valueAfter);
+        node.children = child;
+        //if (node.name === 'setting6') console.log(node.children);
       } else {
         node.diff = 'changed';
       }
     }
     return [...acc, node];
   }, []);
-  return parsedArray;
-}
+  //console.log(parsedArray);
+  return parsedArray; // массив распарсенных узлов
+};
 
 export default (objBefore, objAfter) => {
-
   const parsedArray = parse(objBefore, objAfter);
   const result = render(parsedArray);
-  console.log(result);
-  // const resultStr = JSON.stringify(resultJSON, null, ' ');
-  return result;
+  const result1 = `{\n${result}\n}`;
+  console.log(result1);
+  return result1;
 };

03223fccc1706f8fe28b5f71a63735963861690a
NickKrsk <ggd43@rambler.ru>
2020-02-21 18:12:58 +0700

    refactoring

--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ import plainRender from './formatters/plain-formatter';
 import jsonRender from './formatters/json-formatter';
 
 const getDiffType = (valueBefore, valueAfter) => {
-  if (valueBefore === '') {
+  if (valueBefore === undefined) {
     return 'add';
   }
-  if (valueAfter === '') {
+  if (valueAfter === undefined) {
     return 'remove';
   }
   if (!_.isObject(valueBefore) && !_.isObject(valueAfter)) {
@@ -29,45 +29,22 @@ const compareObjects = (objBefore, objAfter) => {
 
   const keys = _.union(_.keys(objBefore), _.keys(objAfter)).sort();
   return keys.reduce((acc, key) => {
+    const valueBefore = objBefore[key];
+    const valueAfter = objAfter[key];
+    const diffType = getDiffType(valueBefore, valueAfter);
     if (!_.has(objBefore, key)) {
       // 1. key was not found in data1 (add)
-      const valueBefore = '';
-      const valueAfter = objAfter[key];
       const children = compareObjects(valueAfter, valueAfter);
-      const diffType = getDiffType(valueBefore, valueAfter);
-
-      const node = {
-        name: key,
-        valueBefore,
-        valueAfter,
-        diffType,
-        children,
-      };
-      return [...acc, node];
+      //return [...acc, node];
     }
 
     if (!_.has(objAfter, key)) {
       // 2. key was not found in data2` (remove);
-      const valueBefore = objBefore[key];
-      const valueAfter = '';
-      const diffType = getDiffType(valueBefore, valueAfter);
       const children = compareObjects(valueBefore, valueBefore);
-
-      const node = {
-        name: key,
-        valueBefore,
-        valueAfter,
-        diffType,
-        children,
-      };
-      return [...acc, node];
+      //return [...acc, node];
     }
-
     // 3. key was found in both objects
-    const valueAfter = objAfter[key];
-    const valueBefore = objBefore[key];
     const children = compareObjects(valueBefore, valueAfter);
-    const diffType = getDiffType(valueBefore, valueAfter);
 
     const node = {
       name: key,
